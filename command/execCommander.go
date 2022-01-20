df099ed69433c9874dc80adc1e4a7379e826f758
battenworks <david.batten@recursionpharma.com>
2022-01-19 21:50:28 -0700

    remove console output from CommandExecutor because, you know, side effects...

--- command/execCommander.go
+++ command/execCommander.go
@@ -2,9 +2,6 @@ package command
 
 import (
 	"os/exec"
-	"strings"
-
-	"github.com/battenworks/go-common/console"
 )
 
 type CommandExecutor struct{}
@@ -12,17 +9,5 @@ type CommandExecutor struct{}
 // Execute executes the given command with the given arguments
 func (ce CommandExecutor) Execute(cmdName string, cmdArgs ...string) ([]byte, error) {
 	cmd := exec.Command(cmdName, cmdArgs...)
-	out, err := cmd.CombinedOutput()
-
-	console.Out("command: ")
-	console.Yellow(strings.Join(cmd.Args, " ") + " ")
-
-	if err != nil {
-		console.Redln("error")
-		return out, err
-	}
-
-	console.Greenln("success")
-
-	return out, nil
+	return cmd.CombinedOutput()
 }
